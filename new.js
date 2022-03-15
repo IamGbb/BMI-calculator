e6ae6286bb7e696f60b3c724b1d16522cd9c13e8
IamGbb <84709524+IamGbb@users.noreply.github.com>
2022-03-15 15:44:24 +0200

    Delete new.js

--- new.js
+++
@@ -1,7 +0,0 @@
-var animation = bodymovin.loadAnimation({
-  container: document.getElementById('anim'), // Required
-  path: "waiting.json", // Required
-  renderer: 'svg', // Required
-  loop: true, // Optional
-  autoplay: true, // Optional
-})
